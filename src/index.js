ad2ca3263ce343d07a4cac3e57a5e65b5a96da9e
Priyanka Nath <you@example.com>
2024-01-03 15:10:48 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// const root = ReactDOM(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+const root = document.getElementById('root');
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+ </React.StrictMode>,
+  root
+);
+
+
+
